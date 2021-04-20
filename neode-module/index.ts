4ae5413b3a121e095b273070fdc9bf736c5bcbbc
Alessandrodev <alessandroadm@live.com>
2021-04-20 01:03:28 -0300

    fix(log): fix error on warn log

--- neode-module/index.ts
+++ neode-module/index.ts
@@ -2,75 +2,75 @@ import { Global, Module, DynamicModule, Logger } from '@nestjs/common';
 import * as Neode from 'neode';
 
 // Just handle warn on terminal
-const handleWarn = (schema: string)=> new Logger('NeodeModule', true).console.warn(`Could not install schema ${schema}. Already installed ?`);
+const handleWarn = (schema: string) =>
+     new Logger('NeodeModule', true).warn(
+          `Could not install schema ${schema}. Already installed ?`,
+     );
 
 interface Schema {
-  [label: string]: Neode.SchemaObject;
+     [label: string]: Neode.SchemaObject;
 }
 
 @Global()
 @Module({})
 export class NeodeModule {
-  /**
-   * @description ensure you have a .env file with neo4j connection settings
-   * @returns Connection as Neode instance
-   */
-  static forRoot(): DynamicModule {
-    return {
-      module: NeodeModule,
-      global: true,
-      providers: [
-        {
-          provide: 'Connection',
-          useFactory: async () => {
-            let connection: Neode;
+     /**
+      * @description ensure you have a .env file with neo4j connection settings
+      * @returns Connection as Neode instance
+      */
+     static forRoot(): DynamicModule {
+          return {
+               module: NeodeModule,
+               global: true,
+               providers: [
+                    {
+                         provide: 'Connection',
+                         useFactory: async () => {
+                              const connection: Neode = await Neode.fromEnv();
 
-            connection = await Neode.fromEnv();
-          
-            return connection;
-          },
-        }
-      ],
-      exports: ['Connection'],
-    };
-  }
+                              return connection;
+                         },
+                    },
+               ],
+               exports: ['Connection'],
+          };
+     }
 
-  /**
-   *
-   * @param schema key: as label name and value: Node definition as `SchemaObject`
-   * example: `{ User: UserSchema }`
-   * @returns Instance of Neode as connection for module
-   */
-  static forFeature(schema: Schema): DynamicModule {
-    return {
-      module: NeodeModule,
-      global: false,
-      providers: [
-        {
-          provide: 'CONFIG',
-          useValue: schema,
-        },
-        {
-          provide: 'Connection',
-          useFactory: async () => {
-            const connection = await Neode.fromEnv().with(schema);
+     /**
+      *
+      * @param schema key: as label name and value: Node definition as `SchemaObject`
+      * example: `{ User: UserSchema }`
+      * @returns Instance of Neode as connection for module
+      */
+     static forFeature(schema: Schema): DynamicModule {
+          return {
+               module: NeodeModule,
+               global: false,
+               providers: [
+                    {
+                         provide: 'CONFIG',
+                         useValue: schema,
+                    },
+                    {
+                         provide: 'Connection',
+                         useFactory: async () => {
+                              const connection = await Neode.fromEnv().with(
+                                   schema,
+                              );
 
-            // If schema already installed It handle warn
-            try {
-              await connection.schema.install();
-
-            } catch (error) {
-              handleWarn(Object.keys(schema)[0]);
-
-            } finally {
-              return connection;
-
-            }
-          },
-          inject: ['CONFIG'],
-        },
-      ],
-      exports: ['Connection'],
-    };
-  }
+                              // If schema already installed It handle warn
+                              try {
+                                   await connection.schema.install();
+                              } catch (error) {
+                                   handleWarn(Object.keys(schema)[0]);
+                              } finally {
+                                   return connection;
+                              }
+                         },
+                         inject: ['CONFIG'],
+                    },
+               ],
+               exports: ['Connection'],
+          };
+     }
 }
