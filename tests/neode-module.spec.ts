461d60739979f5aef06450f2bf5388367789116a
4lessandrodev <alessandroadm@live.com>
2021-06-20 12:47:24 -0300

    test: added test to check settings types

--- tests/neode-module.spec.ts
+++ tests/neode-module.spec.ts
@@ -1,20 +1,44 @@
-import { NeodeModule } from '../src/index';
+import { IConfig, NeodeModule } from '../src/index';
 import Neode from 'neode';
-import { fromEnv } from 'neode';
+import { fromEnv, SchemaObject } from 'neode';
 
-describe('index', () => {
-     it('module should be defined', () => {
-          const module = new NeodeModule();
-          expect(module).toBeDefined();
-     });
+describe('connection with .env', () => {
+	it('module should be defined', () => {
+		const module = new NeodeModule();
+		expect(module).toBeDefined();
+	});
 
-     it('neode should be installed', () => {
-          const neode = fromEnv;
-          expect(neode).toBeDefined();
-     });
+	it('neode should be installed', () => {
+		const neode = fromEnv;
+		expect(neode).toBeDefined();
+	});
 
-     it('should open a connection from env', () => {
-          const neode: Neode = fromEnv();
-          expect(neode).toBeDefined();
-     });
+	it('should open a connection from env', () => {
+		const neode: Neode = fromEnv();
+		expect(neode).toBeDefined();
+	});
+});
+
+describe('connection with config', () => {
+	const schema: SchemaObject = {
+		id: {
+			type: 'uuid',
+			primary: true,
+			required: true
+		},
+		name: {
+			type: 'string',
+			required: true
+		}
+	};
+	const config: IConfig = {
+		host: 'bolt://localhost',
+		password: 'neo4j',
+		port: 7687,
+		username: 'neo4j'
+	};
+	it('types for connection should be defined', () => {
+		expect(schema).toBeDefined();
+		expect(config).toBeDefined();
+	});
 });
